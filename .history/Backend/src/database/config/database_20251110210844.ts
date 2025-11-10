ed4b25bde59c204d11090ae318b1e184f1d3b014
samratpoudel444 <poudelsamrat444@gmail.com>
2025-11-10 21:29:55 +0545

    fixed

---
+++ .history/Backend/src/database/config/database_20251110210844.ts
@@ -0,0 +1,31 @@
+import Sequelize} from 'sequelize';
+
+const sequelize = new Sequelize(
+  "defaultdb",
+  "avnadmin",
+  "AVNS_IbyTukk7GW0LSCHxWZI",
+  {
+    host: "mysql-8ac4d66-poudelsamrat444-9b9b.j.aivencloud.com",
+    port: 14654,
+    dialect: "mysql",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  }
+);
+
+(async()=>
+{
+    try{
+        await sequelize.authenticate();
+        console.log("Connected to Database sucessfully");
+    }
+    catch(err){
+        console.log("Error Occured while connecting", err)
+    }
+})();
+
+export default sequelize;
---
+++ .history/Backend/src/index_20251110211136.ts
@@ -0,0 +1,28 @@
+// index.ts
+import  express  from "express";
+import dotenv from "dotenv";
+import type { Request, Response } from "./src/node_modules/@types/express";
+
+
+
+
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.use("/api/v1", (req:Request, res:Response)=>
+{
+    console.log("hdhd")
+});
+
+
+app.get("/api/1", (req: Request, res: Response) => {
+  res.status(200).json({ sam: "heklds" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on localhost:${PORT}`);
+});
